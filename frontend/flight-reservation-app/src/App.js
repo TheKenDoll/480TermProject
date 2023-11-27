fc315edfda2b22a5d808bc462c0a3af805e2dbfb
Charlie Lang <charlie.lang@ucalgary.ca>
2023-11-26 21:39:11 -0700

    log/account nav updated

--- frontend/flight-reservation-app/src/App.js
+++ frontend/flight-reservation-app/src/App.js
@@ -1,22 +1,34 @@
 import './App.css';
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './Layout.js';
 import Login from './components/login/Login.js';
 import Landing from './components/login/Landing.js';
 import Account from './components/login/Account.js';
 import Book from './components/bookings/Book.js';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 const App = () => {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
   return (
     <div className="App">
       <Router>
+        <Layout />
+
         <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<Landing />} />
-            </Route>
+          <Route path="/" element={<Landing />} />
+          <Route
+            path="/account"
+            element={
+              isLoggedIn ? (
+                <Account />
+              ) : (
+                <Navigate to="/login" replace={true} />
+              )
+            }
+          />
+
           <Route path="/login" element={<Login />} />
-          <Route path="/account" element={<Account />} />
           <Route path="/book" element={<Book />} />
         </Routes>
       </Router>
